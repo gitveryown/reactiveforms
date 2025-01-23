e6a32bf9f1e18f7fe4145c791e98e14a5181ca15
William Wright <Will@Tashaes-MacBook-Air.local>
2025-01-22 23:52:49 -0500

    updated form validation

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = 'https://localhost:5000/api';
+  private baseUrl: string = 'https://localhost:5000/api';
 
   constructor(private https: HttpClient) {}
 
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,27 +1,47 @@
-import { Component } from '@angular/core';
+import { NgIf, NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import {
   ReactiveFormsModule,
   FormGroup,
   FormControl,
-  EmailValidator,
+  Validators,
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, NgStyle, NgIf],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form!: FormGroup;
+
   constructor(private router: Router) {}
 
-  form = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-  });
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+  get firstName() {
+    return this.form.get('firstName');
+  }
+  get lastName() {
+    return this.form.get('lastName');
+  }
+  get email() {
+    return this.form.get('email');
+  }
 
   onSubmit() {
     const value = this.form.value;
